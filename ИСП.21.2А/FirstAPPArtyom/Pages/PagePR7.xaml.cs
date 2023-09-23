9a2e17b5c96c402e81d3d03a49b42bdda2472810
KarpovArtyom <145660858+KarpovArtyom@users.noreply.github.com>
2023-09-23 14:21:14 +0300

    Add files via upload

--- ИСП.21.2А/FirstAPPArtyom/Pages/PagePR7.xaml.cs
+++ ИСП.21.2А/FirstAPPArtyom/Pages/PagePR7.xaml.cs
@@ -24,19 +24,28 @@ namespace FirstAPPPolina.Pages
         {
             InitializeComponent();
         }
-
+        
         private void btnGo_Click(object sender, RoutedEventArgs e)
         {
             lstResult.Items.Clear();
-            int [] D = { };
-            int [] E = { };
-            int f;
+            lstD.Items.Clear();
+            lstE.Items.Clear();
             lstResult.Items.Add("Лаб.раб.№7. Выполнил Карпов А.М.");
-            for (int i = 0; i <= 17; i = i + 1) ;
-            {
-                f(i) = (2 * D(i) + Math.Sin(E(i))) / D(i);
-            }
+            double[] D = { 1.2, 3.4, 5.6, 7.8, 9.0, 2.1, 4.3, 6.5, 8.7, 0.9, 2.3, 4.5, 6.7, 8.9, 1.0, 3.2 };
+            double[] E = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6 };
 
+
+            for (int i = 0; i < D.Length; i++)
+            {
+                double f = (2 * D[i] + Math.Sin(E[i])) / D[i];
+
+                if (f > 1 && f < 3)
+                {
+                    lstResult.Items.Add($"Результат f={Math.Round(f, 3)}");
+                }
+                lstD.Items.Add($"D(i)={D[i]}");
+                lstE.Items.Add($"E(i)={E[i]}");
+            }
         }
     }
 }
